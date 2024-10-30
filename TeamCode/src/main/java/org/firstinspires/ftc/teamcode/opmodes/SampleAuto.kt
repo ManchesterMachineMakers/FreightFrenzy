346eff4c74c5e72f26c68f72726be732c3a48a6a
milesavernon <milesavernon@gmail.com>
2024-10-29 22:08:25 -0400

    created path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SampleAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SampleAuto.kt
@@ -1,27 +1,71 @@
 package org.firstinspires.ftc.teamcode.opmodes
 
+import com.acmerobotics.dashboard.config.Config
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.firstinspires.ftc.teamcode.pedroPathing.follower.Follower
+import org.firstinspires.ftc.teamcode.pedroPathing.localization.Pose
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierCurve
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.BezierLine
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Path
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathBuilder
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.PathChain
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Point
 import org.firstinspires.ftc.teamcode.subassemblies.MecDriveBase
 import org.firstinspires.ftc.teamcode.subassemblies.Vision
+import org.firstinspires.ftc.teamcode.util.toRadians
+
+/*
+The view is from the bottom of the robot looking upwards.
+left on robot is y pos
+front on robot is x pos
+   /--------------\
+   |     ____     |
+   |     ----     |
+   | ||        || |
+   | ||        || |   left (y pos)
+   |              |
+   |              |
+   \--------------/
+     front (x pos)
+ */
 
 @Autonomous(name = "Sample Autonomous")
 class SampleAuto: LinearOpMode() {
-  
-    override fun runOpMode() {
 
-        val driveBase = MecDriveBase(this)
-        val vision = Vision(this)
-        val follower = Follower(driveBase)
+    val driveBase = MecDriveBase(this)
+    val vision = Vision(this)
+    val follower = Follower(driveBase)
+
+    val pathBuilder = PathBuilder() // path generated with https://pedro-path-generator.vercel.app/
+        .addPath(
+            BezierCurve(
+                Point(startingPosX, startingPosY, Point.CARTESIAN),
+                Point(endingPosX, endingPosY, Point.CARTESIAN),
+            )
+        )
+        .setLinearHeadingInterpolation(startingHeading.toRadians(), endingHeading.toRadians())
+
+    override fun runOpMode() {
 
         vision.aprilTag
         follower.initialize()
+        follower.setStartingPose(Pose(startingPosX, startingPosY, startingHeading.toRadians()))
 
         waitForStart()
 
         if (opModeIsActive()) {
-            
+            follower.followPath(pathBuilder.build())
         }
+    }
+
+    @Config
+    companion object {
+        @JvmField var startingPosX = 176.0
+        @JvmField var startingPosY = -244.0
+        @JvmField var startingHeading = -90.0 // degrees
+        @JvmField var endingPosX = 176.0
+        @JvmField var endingPosY = -244.0
+        @JvmField var endingHeading = 90.0 // degrees
     }
 }
