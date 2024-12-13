2e8615d29450be3594919d9ba356d819a261980c
Aleks Rūtiņš <aleks@rutins.com>
2024-12-13 16:41:08 -0500

    April tag stuff!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AprilTagTestAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AprilTagTestAuto.kt
@@ -1,20 +1,19 @@
-package org.firstinspires.ftc.teamcode
+package org.firstinspires.ftc.teamcode.opmodes
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.firstinspires.ftc.teamcode.subassemblies.Vision
 
-@Autonomous(name = "Empty Autonomous")
-class EmptyAuto: LinearOpMode() {
+@Autonomous(name = "April Tag Test")
+class AprilTagTestAuto: LinearOpMode() {
 
     override fun runOpMode() {
         waitForStart()
-
         val vision = Vision(this)
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                telemetry.addData("April Tag Distance", vision.aprilTagDistance(13))
+                telemetry.addData("Distance to April Tag", vision.aprilTagDistance(13))
                 idle()
             }
         }
