b8644f5635a242a11810dc416263effadc77f9fc
Miles Vernon <milesavernon@gmail.com>
2025-01-24 15:33:22 -0500

    claw servo changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassemblies/AltClaw.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassemblies/AltClaw.kt
@@ -8,20 +8,23 @@ import org.firstinspires.ftc.teamcode.util.Subassembly
 
 class AltClaw(opMode: OpMode) : Subassembly(opMode, "Alt Claw") {
 
-    val clawServo = ReleaseServo(hardwareMap.servo.get("alt_claw"), Pair(0.1, 0.8))
+    val clawServo = ReleaseServo(hardwareMap.servo.get("alt_claw"), Pair(0.0, 0.3))
     val rotateServo = hardwareMap.servo.get("alt_rotate")
 
     init {
-//        clawServo.direction = Servo.Direction.REVERSE
+        clawServo.direction = Servo.Direction.REVERSE
 //        rotateServo.direction = Servo.Direction.REVERSE
     }
 
     fun control(gamepad: Gamepad) {
-        if (gamepad.a) clawServo.open()
-        if (gamepad.y) clawServo.close()
+        if (gamepad.a) clawServo.close()
+        if (gamepad.y) clawServo.open()
 
         rotateServo.position += gamepad.right_stick_y * rotateServoCoefficient
     }
+
+    fun open() = clawServo.open()
+    fun close() = clawServo.close()
 
     companion object {
         @JvmField var rotateServoCoefficient = 0.003 // this value should be the highest possible without the pinion overshooting it's controls
