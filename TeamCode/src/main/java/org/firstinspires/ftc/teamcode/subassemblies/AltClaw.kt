a3eb07c83c37c28c7372135c1a75ed088f204847
milesavernon <milesavernon@gmail.com>
2025-01-25 10:52:10 -0500

    fixed servo ranges, added FTCDash support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassemblies/AltClaw.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassemblies/AltClaw.kt
@@ -1,14 +1,16 @@
 package org.firstinspires.ftc.teamcode.subassemblies
 
+import com.acmerobotics.dashboard.config.Config
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.teamcode.util.ReleaseServo
 import org.firstinspires.ftc.teamcode.util.Subassembly
 
+@Config
 class AltClaw(opMode: OpMode) : Subassembly(opMode, "Alt Claw") {
 
-    val clawServo = ReleaseServo(hardwareMap.servo.get("alt_claw"), Pair(0.0, 0.3))
+    val clawServo = ReleaseServo(hardwareMap.servo.get("alt_claw"), Pair(servoRangeMin, servoRangeMax))
     val rotateServo = hardwareMap.servo.get("alt_rotate")
 
     init {
@@ -28,5 +30,7 @@ class AltClaw(opMode: OpMode) : Subassembly(opMode, "Alt Claw") {
 
     companion object {
         @JvmField var rotateServoCoefficient = -0.003 // this value should be the highest possible without the pinion overshooting it's controls
+        @JvmField var servoRangeMin = 0.5
+        @JvmField var servoRangeMax = 0.725
     }
 }
